224e26d4e7804990807f10cdeefbd3b9deed954a
cf904d31 <cf904d31@gmail.com>
2016-09-09 11:15:19 +0800

    20160909樂透測試

---
+++ app/src/main/java/iii/org/tw/a20160909lotto/MainActivity.java
@@ -0,0 +1,13 @@
+package iii.org.tw.a20160909lotto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
