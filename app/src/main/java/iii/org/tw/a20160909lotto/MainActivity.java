4e763b99fcd97317d885e0011d0c5ae83cbbadae
cf904d31 <cf904d31@gmail.com>
2016-09-09 12:08:12 +0800

    20160909樂透測試(按按鈕)顯示一個隨機號碼

--- app/src/main/java/iii/org/tw/a20160909lotto/MainActivity.java
+++ app/src/main/java/iii/org/tw/a20160909lotto/MainActivity.java
@@ -3,13 +3,31 @@ package iii.org.tw.a20160909lotto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnClick;
+    private TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnClick = (Button)findViewById(R.id.btnClick);
+        showText = (TextView)findViewById(R.id.showText);
+
         Log.i("Abner","Test");
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showText.setText("樂透號碼:" + (int)(Math.random()*49+1));
+            }
+        });
+    }
+    public void click(View v) {
+        Log.i("Abner","Click()");
     }
 }
